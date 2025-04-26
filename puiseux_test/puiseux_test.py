1f6181857cee099056cb91d836c94dcbda78fb51
piotrmgs <75215093+piotrmgs@users.noreply.github.com>
2025-04-26 17:52:42 +0200

    Update puiseux_test.py

--- puiseux_test/puiseux_test.py
+++ puiseux_test/puiseux_test.py
@@ -1,3 +1,6 @@
+# Copyright (c) 2025 Piotr Migus
+# This code is licensed under the MIT License.
+# See the LICENSE file in the repository root for full license information.
 import sympy
 import os
 from sympy import nroots, symbols, expand, nsimplify, I
