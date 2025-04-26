8ad69d417a2e51cbd22aac5de183d09e6e2008d0
piotrmgs <75215093+piotrmgs@users.noreply.github.com>
2025-04-26 17:52:21 +0200

    Update post_processing_synth.py

--- post_processing_synth/post_processing_synth.py
+++ post_processing_synth/post_processing_synth.py
@@ -1,3 +1,6 @@
+# Copyright (c) 2025 Piotr Migus
+# This code is licensed under the MIT License.
+# See the LICENSE file in the repository root for full license information.
 import numpy as np
 import torch
 import random
